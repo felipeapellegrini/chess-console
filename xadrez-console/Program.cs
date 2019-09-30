deba3de3aba36f2b11b981bda4d0c738e1524180
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2019-09-29 23:01:09 -0300

    Melhorando a impressao das pecas no tabuleiro (diferenciando cores)

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,9 +8,21 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('C', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            Console.WriteLine();
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
